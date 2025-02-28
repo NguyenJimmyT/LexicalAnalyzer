d1a41d986fe93517358ba3f595c141cbd2777c26
Leah Trinh <40077112+ldt4me@users.noreply.github.com>
2025-02-28 11:36:24 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-keywords = ['if', 'endif', 'while']
+keywords = ['if', 'endif', 'while', 'endwhile', 'else', 'int', 'float', 'string', 'return', 'def', 'elif', 'print', 'input', 'for', 'in', 'range', 'break', 'continue', 'and', 'or', 'not', 'True', 'False', 'None']
+accepted_states = ['endwhile','break', 'pass','continue']
+operators = ['+', '-', '*', '/', '%', '==', '!=', '>', '<', '>=', '<=', '+=', '-=', '*=', '/=', '%=', '&&', '||', "="]
+separators = ['(', ')', '{', '}', '[', ']', ',', ':', ';']
+identifiers = []
+real = []
+
