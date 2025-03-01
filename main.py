090a19d4e6b2574b279410baa631dffcb4003330
Leah Trinh <40077112+ldt4me@users.noreply.github.com>
2025-03-01 10:40:32 -0800

    token class should be done, lexer is not

--- main.py
+++ main.py
@@ -5,3 +5,22 @@ separators = ['(', ')', '{', '}', '[', ']', ',', ':', ';']
 identifiers = []
 real = []
 
+class Token: 
+    def __init__(self, token_type, token_value):
+        self.token_type = token_type
+        self.token_value = token_value
+    
+    def __str__(self):
+        return f"Token({self.token_type}, {self.token_value})"
+
+class Lexical_Analysis:
+    def _init__(self, code):
+        self.code = code
+        self.index = 0 
+        self.tokens = []
+        self.current_token = None
+        self.current_char = self.code[self.index]
+    
+    def forward(self):
+        self.index += 1
+        self.current_char = self.code[self.index] if self.index < len(self.code) else None
